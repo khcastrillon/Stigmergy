9fd0af86e8e700728fb5d8eb3d0f1c1fad69a4b5
khcastrillon <kh.castrillon@uniandes.edu.co>
2021-11-19 23:20:31 -0500

    MQTT + Encript

--- Orden/views.py
+++ Orden/views.py
@@ -4,7 +4,51 @@ from .logic.logic_orden import update_estado_orden
 from .logic.logic_orden import create_Orden
 from django.http import HttpResponse
 from django.core import serializers
+import rsa
 import json
+import random
+from paho.mqtt import client as mqtt_client
+
+
+publicKey, privateKey = rsa.newkeys(512)
+topicOrden, topicSAP = "Orden", "SAP"
+
+broker = 'broker.emqx.io'
+port = 1883
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+username = 'SAP'
+password = 'public'
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
+
+client = mqtt_client.Client(client_id)
+client.username_pw_set(username, password)
+client.on_connect = on_connect
+client.connect(broker, port)
+
+# Se suscribe a ordenes
+def on_message(client, userdata, msg):
+    print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+client.subscribe(topicSAP)
+client.on_message = on_message
+
+# Publica las actualizaciones de SAP
+def publish(client, msg):
+    result = client.publish(topicOrden, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        print(f"Send `{msg}` to topic `{topicOrden}`")
+    else:
+        print(f"Failed to send message to topic {topicSAP}")
+
+publicKey, privateKey = rsa.newkeys(512)
+topicOrden, topicSAP = "Orden", "SAP"
 
 @csrf_exempt
 def ordenAPI(request,id=0):
@@ -16,6 +60,7 @@ def ordenAPI(request,id=0):
         data = json.loads(request.body)
         orden = [data['fields']['nombre'], data['fields']['articulos'], data['fields']['estado'], data['fields']['valor']]
         create_Orden(orden)
+        mqtt.publish()
         return HttpResponse("Orden creada en SAP satisfactoriamente")
     elif request.method == 'PUT':
         data = json.loads(request.body)
--- SAP/views.py
+++ SAP/views.py
@@ -2,9 +2,49 @@ import json
 from .logic import logic_SAP
 from django.views.decorators.csrf import csrf_exempt
 from Orden.views import update_orden_view, get_orden_view
+import random
+from paho.mqtt import client as mqtt_client
 from django.http import HttpResponse
 from django.core import serializers
 import time
+import rsa
+
+publicKey, privateKey = rsa.newkeys(512)
+topicOrden, topicSAP = "Orden", "SAP"
+
+broker = 'broker.emqx.io'
+port = 1883
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+username = 'SAP'
+password = 'public'
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker!")
+    else:
+        print("Failed to connect, return code %d\n", rc)
+
+client = mqtt_client.Client(client_id)
+client.username_pw_set(username, password)
+client.on_connect = on_connect
+client.connect(broker, port)
+
+# Se suscribe a ordenes
+def on_message(client, userdata, msg):
+    print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+client.subscribe(topicOrden)
+client.on_message = on_message
+
+# Publica las actualizaciones de SAP
+def publish(client, msg):
+    result = client.publish(topicSAP, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status == 0:
+        print(f"Send `{msg}` to topic `{topicSAP}`")
+    else:
+        print(f"Failed to send message to topic {topicSAP}")
 
 estadosPosibles = ('En HUB de despacho', 'Dron asignado', 'Dron despachado', 'Volando posición X1,Y1', 'Volando posición X2, Y2', 'Aterrizando en HUB de llegada', 'En HUB de llegada')
 
